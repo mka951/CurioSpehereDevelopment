24546b2d42116574b17beec1c664cebaebd1d480
mhmddnoo <marcellwilliam123@gmail.com>
2024-11-18 17:22:30 +0700

    e

--- script.js
+++ script.js
@@ -1,9 +1,35 @@
-var styles = {
+let style1 = {
+    opacity: "0",
+};
+
+let style2 = {
+    opacity: "1",
+    transitionDuration: "1s",
+};
+
+let style3 = {
     opacity: "0",
     transitionDuration: "1s",
 };
 
+let style4 = {
+    visibility: "hidden",
+};
+
 var welcomeElement = document.getElementById("welcome");
-welcomeElement.onclick = function() {
-    Object.assign(welcomeElement.style, styles);
+window.onload = function() {
+
+    Object.assign(welcomeElement.style, style1);
+
+    setTimeout(function() {
+        Object.assign(welcomeElement.style, style2);
+    }, 500);
+
+    setTimeout(function() {
+        Object.assign(welcomeElement.style, style3)
+    },3000)
+
+    setTimeout(function() {
+        Object.assign(welcomeElement.style, style4)
+    },3001)
 };

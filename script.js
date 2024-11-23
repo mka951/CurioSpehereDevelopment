38f2e7f76f358de0e7d5c693621bdc73e33cf11a
Seseorang <134702987+mka951@users.noreply.github.com>
2024-11-23 21:05:42 +0700

    Fixing things
    -Fixed messy css
    -Fixed background fading with text in welcome
    -Shortened some classes and ids

--- script.js
+++ script.js
@@ -16,20 +16,27 @@ let style4 = {
     visibility: "hidden",
 };
 
-var welcomeElement = document.getElementById("welcome");
+const wElement = document.getElementById("wtext");
+const wElementBg = document.getElementById("wsec");
 window.onload = function() {
 
-    Object.assign(welcomeElement.style, style1);
+    Object.assign(wElement.style, style1);
 
     setTimeout(function() {
-        Object.assign(welcomeElement.style, style2);
-    }, 500);
+        Object.assign(wElement.style, style2);
+    }, 1000);
 
     setTimeout(function() {
-        Object.assign(welcomeElement.style, style3)
-    },3000)
+        Object.assign(wElement.style, style3)
+    },3500)
+    setTimeout(function() {
+        Object.assign(wElementBg.style, style3)
+    },4000)
 
     setTimeout(function() {
-        Object.assign(welcomeElement.style, style4)
-    },3001)
+        Object.assign(wElement.style, style4)
+    },4500)
+    setTimeout(function() {
+        Object.assign(wElementBg.style, style4)
+    },5000)
 };
